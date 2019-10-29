0ebb0ff553b05592a628ffc6f8a7e78830a9ff0d
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-10-29 22:29:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,8 +39,12 @@ def monopoly_with_third_tier
 	    :four_pieces_owned => 200
 	  },
 	  :names => {
-	    :reading_railroad => {},
-	    :pennsylvania_railroad => {},
+	    :reading_railroad => {
+	      "mortgage_value" => "100"
+	    },
+	    :pennsylvania_railroad => {
+	      "mortgage"
+	    },
 	    :b_and_o_railroad => {},
 	    :shortline => {}
 	  }
